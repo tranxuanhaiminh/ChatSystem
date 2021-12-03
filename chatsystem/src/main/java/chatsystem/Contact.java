06bc60446c62c10043c6a4cb30936cb7e3ec322b
marietousarr <marietou60@gmail.com>
2021-12-03 14:49:33 +0100

    "connection interface"

---
+++ chatsystem/src/main/java/chatsystem/Contact.java
@@ -0,0 +1,31 @@
+package chatsystem;
+
+public class Contact {
+	
+	private String pseudo;
+	private String ipaddress;
+	
+	
+	public Contact() {
+		this.pseudo = null;
+		this.ipaddress = null;
+	}
+	
+	public Contact(String p, String ip) {
+		this.pseudo = p;
+		this.ipaddress = ip;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getIpaddress() {
+		return ipaddress;
+	}
+	
+}
