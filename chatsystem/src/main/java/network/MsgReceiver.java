b999b4cc91fb99bb20575e7716aa7c5ddf873569
marietousarr <marietou60@gmail.com>
2022-01-17 15:28:12 +0100

    base de données msg recu modif

--- chatsystem/src/main/java/network/MsgReceiver.java
+++ chatsystem/src/main/java/network/MsgReceiver.java
@@ -52,7 +52,7 @@ public class MsgReceiver extends Thread{ // Server tcp //client tcp
 					
 					//adding to the chat history
 			    	conv.getMain().getConDB().insertChat(mess.getDest().getIpaddress().getHostAddress(), mess.toString(), mess.convertDateToFormat(), false);
-			    	System.out.println("Adding the msg sent to you "+ mess.getDest().getIpaddress().getHostAddress()+" to the chat history\n");
+			    	System.out.println("Adding the msg sent to you "+ conv.getInterlocutor().getIpaddress().getHostAddress()+" to the chat history\n");
 			    	
 				} catch (ClassNotFoundException e1) {
 					e1.printStackTrace();
